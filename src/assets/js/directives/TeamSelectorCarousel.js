901ec9e9484ce8243762beb3c2f47399ee712c44
alexfaunt <alexfaunt@gmail.com>
2014-07-18 18:00:00 +0100

    Added kinetic scrolling, needs redoing and making into a idrective when i cba

--- src/assets/js/directives/TeamSelectorCarousel.js
+++ src/assets/js/directives/TeamSelectorCarousel.js
@@ -3,6 +3,8 @@
  * Team selector carousel
  */
 
+ // HOLY SHIT THIS NEEDS REWRITING
+
 ;(function (window, angular) {
     'use strict';
 
@@ -12,86 +14,137 @@
 
         var TeamSelectorCarousel = function ($scope, $element) {
             var maxIndex;
-            // console.log($element);
 
             var touchStartPageX = null;
             var carouselWrapper = null;
+            var startingPosition = 0;
+            var delta = 0;
+
+            var amplitude, velocity, timestamp, now, elapsed, ticker, trackChange, trackLast, target;
+            var pressed = false;
+
+            var max = -800;
+            var newOffset;
+            var time = 100;
 
             var touchStart = function (e) {
-                touchStartPageX = e.touches[0].pageX;
-                // carouselWrapper = $element.find('section');
-                carouselWrapper = $element[0].querySelector('.slide-wrapper');
-                // angular.element(elem.querySelector('.classname'))
+                pressed = true;
+                touchStartPageX = e.touches[0].pageY;
+                carouselWrapper = $element[0].querySelector('.teams');
+
+                delta = 0;
+                velocity = 0;
+                clearInterval(ticker);
+                ticker = setInterval(track, time);
+
+                trackLast = 0;
+                trackChange = 0;
 
                 $element.bind('touchmove', touchMove);
                 $element.bind('touchend', touchEnd);
             };
 
             var touchMove = function (e) {
-                var diffX = e.touches[0].pageX - touchStartPageX;
-                carouselWrapper.style.cssText = '-webkit-transform: translateX(' + diffX + 'px)';
+                e.preventDefault();
+
+                delta = e.touches[0].pageY - touchStartPageX;
+
+                newOffset = startingPosition + delta;
+                newOffset = newOffset >= 0 ? newOffset/2 : newOffset;
+                newOffset = newOffset <= max ? (max + (newOffset-max)/2) : newOffset;
+
+                carouselWrapper.style.cssText = '-webkit-transform: translate3d(0, ' + newOffset + 'px, 0)';
             };
 
-            var touchEnd = function (e) {
+            var track = function () {
+                // now = Date.now();
+                trackChange = delta - trackLast;
+                trackLast = delta;
+
+                var v = 1000 * trackChange / time;
+                velocity = 0.8 * v + 0.2 * velocity;
+            };
+
+            var touchEnd = function () {
+                startingPosition = newOffset;
+                pressed = false;
+
+                clearInterval(ticker);
+                timestamp = Date.now();
+
+                amplitude = velocity * 0.8;
+                target = newOffset + amplitude;
+
+                if (target > 0) {
+                    target = 0;
+                    amplitude = -newOffset;
+                }
+                if (target < max) {
+                    target = max;
+                    amplitude = max - newOffset;
+                }
+
+                autoScroll();
 
                 $element.unbind('touchmove', touchMove);
                 $element.unbind('touchend', touchEnd);
             };
 
-            $element.bind('touchstart', touchStart);
+            var autoScroll = function () {
+                if (pressed) {
+                    return;
+                }
+                elapsed = Date.now() - timestamp;
+                delta = -amplitude * Math.exp(-elapsed / 200);
+                // /console.log('amplitude',amplitude,'delta',delta,'target',target,'newOffset',newOffset,'autoscrolloffset', (target + delta));
+                if (delta > 1 || delta < -1) {
+                    carouselWrapper.style.cssText = '-webkit-transform: translate3d(0, ' + (target + delta) + 'px, 0)';
+                    window.requestAnimationFrame(autoScroll);
+                }
+                else {
+                    carouselWrapper.style.cssText = '-webkit-transform: translate3d(0, ' + (target) + 'px, 0)';
+                }
+                startingPosition = target + delta;
+            };
 
-            $scope.activeSlides = [0, 1, 3, 4, 5];
+            $element[0].querySelector('.teams').addEventListener('touchstart', touchStart);
 
-            maxIndex = $scope.activeSlides.length - 1;
+            $scope.activeTeams = [0, 1, 3, 4, 5];
 
-            $scope.slides = [
+            maxIndex = $scope.activeTeams.length - 1;
+
+            $scope.teams = [
                 {
                     image: 'http://placekitten.com/400/400',
-                    title: 'Slide One',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide One'
                 },
                 {
                     image: 'http://placekitten.com/500/500',
-                    title: 'Slide Two',
-                    body: 'Stats or something',
-                    active: true
+                    name: 'Slide Two'
                 },
                 {
                     image: 'http://placekitten.com/300/300',
-                    title: 'Slide Three',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide Three'
                 },
                 {
                     image: 'http://placekitten.com/450/450',
-                    title: 'Slide Four',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide Four'
                 },
                 {
                     image: 'http://placekitten.com/330/330',
-                    title: 'Slide Five',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide Five'
                 },
                 {
                     image: 'http://placekitten.com/300/300',
-                    title: 'Slide Three',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide Three'
                 },
                 {
                     image: 'http://placekitten.com/450/450',
-                    title: 'Slide Four',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide Four'
                 },
                 {
                     image: 'http://placekitten.com/330/330',
-                    title: 'Slide Five',
-                    body: 'Stats or something',
-                    active: false
+                    name: 'Slide Five'
                 }
             ];
 
